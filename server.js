a68ae18d8b3a09b1653c91a28823685883c85e3d
Chachulani Maluleke <chachulani.maluleke@dadevs.co.za>
2022-05-19 15:32:05 +0200

    server modified

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/portfolio'));
+app.use(express.static(__dirname + '/dist/Personal-Portfolio'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));})
+res.sendFile(path.join(__dirname+'/dist/Personal-Portfolio/index.html'));})
 app.listen(process.env.PORT || 4200);
